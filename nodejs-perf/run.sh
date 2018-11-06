04b0e9f46baa21d8e2f56c8d144735404deacee8
unclebean <unclebean2013@gmail.com>
2018-11-06 13:46:39 +0800

    try to resolve perf permission issue v6

--- nodejs-perf/run.sh
+++ nodejs-perf/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --mount type=bind,source="$(pwd)"/reports,target=/root/reports heou/nodejs-perf:1.0
+docker run -it --mount type=bind,source="$(pwd)"/reports,target=/root/reports --security-opt seccomp="$(pwd)"/profile.json heou/nodejs-perf:1.0
